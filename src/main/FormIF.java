b7f83b26076ef9f0bfffa0b717f22dcc13ab27d0
laurijko <laurijko@U-423-31.edu.wks.metropolia.fi>
2018-03-12 11:56:36 +0200

    created inteface for forms

---
+++ src/main/FormIF.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author laurijko
+ */
+public interface FormIF<T> {
+    abstract T create();
+    abstract void delete(T entity);
+    abstract T[] search();
+    abstract T update(T entity);
+}
