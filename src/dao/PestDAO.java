bd8a2702d71a1c54406f04f51819a599fd52e142
Xpdmk <roy.jarvinen@gmail.com>
2018-03-12 11:10:04 +0200

    Added PestDAO

---
+++ src/dao/PestDAO.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import orm.Pest;
+
+/**
+ *
+ * @author royja
+ */
+public class PestDAO extends DAO {
+
+    public PestDAO(EntityManager em) {
+        super(em);
+    }
+    
+    public List<Pest> getPestList(){
+        Query q = em.createQuery("SELECT p FROM Pest p");
+        List<Pest> list = q.getResultList();
+        return list;
+    }
+    
+    public Pest getPestById(int id){
+        return em.find(Pest.class, id);
+    }
+    
+    public List<Pest> getPestByName(String name) {
+        String lause = "SELECT p FROM Pest p WHERE p.name = :name";
+        Query kysely = em.createQuery(lause);
+        kysely.setParameter("name", name);
+        return kysely.getResultList();
+    }
+    
+    public void addPest(Pest pest){
+        em.persist(pest);
+    }
+}
