976e24c207fa0a0ae38a61ebee974e6909948d3d
Xpdmk <roy.jarvinen@gmail.com>
2018-03-13 12:58:11 +0200

    Implemented update menu

--- src/main/CustomerVisitForm.java
+++ src/main/CustomerVisitForm.java
@@ -25,6 +25,7 @@ public class CustomerVisitForm implements FormIF<CustomerVisit> {
     private CustomerForm cForm;
     private AddressForm aForm;
     private DateForm dForm;
+    private PestForm pForm;
 
     public CustomerVisitForm(EntityManager em) {
         lukija = new Scanner(System.in);
@@ -32,6 +33,7 @@ public class CustomerVisitForm implements FormIF<CustomerVisit> {
         cForm = new CustomerForm(em);
         aForm = new AddressForm(em);
         dForm = new DateForm();
+        pForm = new PestForm(em);
     }
     
     @Override
@@ -67,6 +69,41 @@ public class CustomerVisitForm implements FormIF<CustomerVisit> {
     
     @Override
     public CustomerVisit update(CustomerVisit customerVisit){
+        String[] fields = {"Päiväys", "Asiakas", "Osoite", "Tuholaiset", "Peruuta"};
+        String input = "";
+        int valinta = 0;
+        do {
+            for (int i = 0; i < fields.length; i++) {
+                System.out.println((i+1) + ". " + fields[i]);
+            }
+            try {
+                input = lukija.nextLine();
+                valinta = Integer.parseInt(input);
+            }catch(Exception e){
+                //Ei numero syöte
+            }
+            
+            switch(valinta){
+                case 1: //Etunimi
+                    System.out.println("Edellinen päiväys: " + customerVisit.getDatetime());
+                    customerVisit.setDatetime(dForm.create());
+                case 2: //Sukunimi
+                    System.out.println("Edellinen asiakas: " + customerVisit.getCustomer());
+                    customerVisit.setCustomer(cForm.create());
+                    break;
+                case 3: //Laskutusosoite
+                    System.out.println("Edellinen osoite: " + customerVisit.getAddress());
+                    customerVisit.setAddress(aForm.create());
+                    break;
+                case 4:
+                    customerVisit.setPests(pForm.update(customerVisit.getPests()));
+                    break;
+                case 5:
+                    return null;
+            }
+            
+        } while(valinta < 1 || valinta > fields.length);
+        cvDAO.addCustomerVisit(customerVisit);
         return customerVisit;
     }
 }
