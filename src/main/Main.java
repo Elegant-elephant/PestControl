6c2baf4c907037e87a7bd5bd8242e1f68e16c556
Xpdmk <roy.jarvinen@gmail.com>
2018-03-13 11:59:16 +0200

    Changed message from search to list to match it's purpose

--- src/main/Main.java
+++ src/main/Main.java
@@ -30,7 +30,7 @@ public class Main {
     
     public static void main(String[] args) {
         String[] mainMenu = {"Asiakas", "Asiakaskäynti", "Osoite", "Tuholainen", "Lopeta"};
-        String[] tableOperations = {"Luo uusi", "Etsi", "Peruuta"};
+        String[] tableOperations = {"Luo uusi", "Listaa", "Peruuta"};
         String[] entityOperations = {"Muokkaa", "Poista", "Peruuta"};
         
         int valinta = 0;
